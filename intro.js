dd60c52e6d5fa07e4d07f45fb1d504b9933fa28f
Erjon Lusha <lushae007@gmail.com>
2021-10-25 10:37:52 +0100

    again and again

--- intro.js
+++ intro.js
@@ -11,7 +11,55 @@
 //npm init (step by step, press enter to skip)
 //npm init -y (everything default)
 
-setInterval(() =>{
-    console.log('hello world');
-}, 2000)
-console.log('i will run first');
+
+const {readFile, writeFile} = require('fs').promises
+//or you can use the method commented below
+//const util = require('util')
+//const readFilePromise = util.promisify(readFile)
+//const writeFilePromise = util.promisify(writeFile)
+
+const start = async()=>{
+    try {
+        const first = await readFile('./content/first.txt', 'utf8')
+        const second = await readFile('./content/second.txt', 'utf8')
+        await writeFile('./content/result-mind-grnade.txt',
+         `THis is okey: ${first} ${second}`,
+         {flag: 'a'}
+         )
+        console.log(first, second);
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+start()
+
+
+//const getTxt = (path) =>{
+    //return new Promise((resolve, reject) =>{
+        //readFile(path, 'utf8', (err, data)=>{
+            //if(err){
+            //    reject(err);
+          //  }else{
+        //        resolve(data)
+      //      }
+        
+    //    })
+
+  //  })
+//}
+
+
+
+
+
+//getTxt('./content/first.txt')
+//.then(result =>console.log(result)
+//.catch(err=> console.log(err)))
+
+
+
+
+
+
